6fd7677e4093ac136928b9ec6b8d7f3ea2710ca9
zwen <18514430012@163.com>
2018-09-28 14:55:45 +0800

    图片消息回复

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -108,12 +108,33 @@ class IndexController extends CommonController
 
     public function receiveImage($postObj)
     {
-        $img = "<img src='./uploads/bq.jpg'>";
+        $access_token = $this->getAccsenToken();
+        $url = "http://file.api.weixin.qq.com/cgi-bin/media/upload?access_token=".$access_token."&type=image";
+        $data =array("media" => "@"."rhlq.jpg");
+        $result = $this->https_request ( $url, $data );
+        var_dump ( $result );
+        /*$img = "<img src='./uploads/bq.jpg'>";
         $tousername = $postObj->FromUserName;
         $fromusername = $postObj->ToUserName;
         $time = time();
         $template = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[image]]></MsgType><Image><MediaId><![CDATA[%s]]></MediaId></Image></xml>";
-        return sprintf($template, $tousername, $fromusername, $time, $img);
+        return sprintf($template, $tousername, $fromusername, $time, $img);*/
+    }
+
+    function https_request($url, $data = null) {
+        $curl = curl_init ();
+        //curl_setopt ( $curl, CURLOPT_SAFE_UPLOAD, false); 
+        curl_setopt ( $curl, CURLOPT_URL, $url );
+        curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, FALSE );
+        curl_setopt ( $curl, CURLOPT_SSL_VERIFYHOST, FALSE );
+        if ( !empty ( $data )) {
+            curl_setopt ( $curl, CURLOPT_POST, 1 );
+            curl_setopt ( $curl, CURLOPT_POSTFIELDS, $data );
+        }
+        curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, 1 );
+        $output = curl_exec ( $curl );
+        curl_close ( $curl );
+        return $output;
     }
 
     /*
